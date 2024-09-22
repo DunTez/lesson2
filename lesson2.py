e00242d76f269ee6890f1cd4bb51166a75bfe261
DunTez <brizhik1998@gmail.com>
2024-09-22 15:12:50 +0200

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,5 @@
+number_of_tasks = 12
+time_spent = (1.5)
+name = 'Phyton'
+time_per_task = (0.125)
+print('Курс:', name,',' 'всего задач:',number_of_tasks,',''затрачено часов:',time_spent,',''среднее время выполнения',time_per_task,'часа.')
